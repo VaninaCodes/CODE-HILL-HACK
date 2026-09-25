4a2974f75334c97dae167fe8edfa92dc10234f96
dorregoagus <dorregoagus18@gmail.com>
2026-09-24 22:59:21 -0300

    Se creó el middleware de autenticación

---
+++ backend/src/app.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+import authRoutes from "./routes/auth.routes.js";
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "API del hackatón funcionando.",
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Servidor funcionando en http://localhost:${PORT}`);
+});
